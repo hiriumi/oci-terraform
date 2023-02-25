5a08d29c8e4428442bd2abb2416bd62b3feee8aa
Hayato Iriumi <hiriumi@gmail.com>
2023-02-24 17:40:56 -0800

    inital commit

--- main.tf
+++ main.tf
@@ -18,3 +18,11 @@ resource "oci_core_vcn" "ashburn_vcn" {
   compartment_id = "ocid1.tenancy.oc1..aaaaaaaaca7lntmtszny3mgfhmzb5jb5oi6xucnrb7z6emorxdzheos4m4pa"
   display_name   = "My internal VCN"
 }
+
+resource "oci_core_subnet" "ashburn" {
+  vcn_id                      = oci_core_vcn.ashburn_vcn.id
+  cidr_block                  = "10.0.0.0/24"
+  compartment_id              = oci_core_vcn.ashburn_vcn.compartment_id
+  display_name                = "My main subnet"
+  prohibit_public_ip_on_vnic  = true
+}
